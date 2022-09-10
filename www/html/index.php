0986b2ffe548b921b94ca407c7428f24734cb340
Matija Kovacevic <matija.kovacevic@typeqast.com>
2022-09-10 17:49:01 +0200

    add connection example for php -> mysql connection

--- www/html/index.php
+++ www/html/index.php
@@ -1,3 +1,23 @@
 <?php
 
 echo 'It works! PHP version: ' . phpversion();
+
+$database ="mydb";  
+$user = "myuser";  
+$password = "password";  
+$host = "db";  
+
+$connection = new PDO("mysql:host={$host};dbname={$database};charset=utf8", $user, $password);  
+$query = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_TYPE='BASE TABLE'");  
+$tables = $query->fetchAll(PDO::FETCH_COLUMN);  
+
+if (empty($tables)) {
+  echo "<p>There are no tables in database \"{$database}\".</p>";
+} else {
+  echo "<p>Database \"{$database}\" has the following tables:</p>";
+  echo "<ul>";
+    foreach ($tables as $table) {
+      echo "<li>{$table}</li>";
+    }
+  echo "</ul>";
+}
